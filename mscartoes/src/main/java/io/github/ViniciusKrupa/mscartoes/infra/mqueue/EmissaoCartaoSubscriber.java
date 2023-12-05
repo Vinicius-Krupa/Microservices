b525a6131a27862450cbccd332272e3e97446073
vsoa <vinicius.oliveira1@gft.com>
2023-12-05 14:10:38 -0300

    Finalizando todos microserviços

--- mscartoes/src/main/java/io/github/ViniciusKrupa/mscartoes/infra/mqueue/EmissaoCartaoSubscriber.java
+++ mscartoes/src/main/java/io/github/ViniciusKrupa/mscartoes/infra/mqueue/EmissaoCartaoSubscriber.java
@@ -8,12 +8,14 @@ import io.github.ViniciusKrupa.mscartoes.domain.DadosSolicitacaoEmissaoCartao;
 import io.github.ViniciusKrupa.mscartoes.infra.repository.CartaoRepository;
 import io.github.ViniciusKrupa.mscartoes.infra.repository.ClienteCartaoRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class EmissaoCartaoSubscriber {
 
     private final CartaoRepository cartaoRepository;
@@ -37,7 +39,7 @@ public class EmissaoCartaoSubscriber {
 
             clienteCartaoRepository.save(clienteCartao);
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("Erro ao receber solicitação de emissão de cartão: {} ", e.getMessage());
         }
     }
 }
